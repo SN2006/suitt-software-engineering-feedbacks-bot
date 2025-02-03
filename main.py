7a64362e7cb2dcaae89d8755cf6e953a51858888
SN2006 <slavanedelev@gmail.com>
2025-02-03 10:48:14 +0200

    Add menu and menu image

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from aiogram import *
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup, \
+    KeyboardButton
 from dotenv import *
 from keep_alive import keep_alive
 
@@ -21,12 +22,19 @@ disp = Dispatcher()
 
 main_keyboard_markup = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text="Сайт уныверситету", url="https://suitt.edu.ua/")],
+        [InlineKeyboardButton(text="Сайт університету", url="https://suitt.edu.ua/")],
         [InlineKeyboardButton(text="Освітня програма", url="https://suitt.edu.ua/wp-content/uploads/2023/12/121-OPP-2-13-04-B.pdf")],
         [InlineKeyboardButton(text="Навчальний план", url="https://suitt.edu.ua/wp-content/uploads/2023/11/NP_B_121_2023.pdf")],
         [InlineKeyboardButton(text="Силабуси", url="https://suitt.edu.ua/sylabusy-121-inzheneriia-prohramnoho-zabezpechennia-bakalavr/")],
         [InlineKeyboardButton(text="Відгуки", url="https://suitt-software-engineering-feedbacks.pages.dev/")],
         [InlineKeyboardButton(text="Залишити відгук", callback_data="feedback")],
+    ]
+)
+
+menu_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    keyboard=[
+        [KeyboardButton(text="☰ Меню ☰")]
     ]
 )
 
@@ -45,9 +53,18 @@ async def save_feedback(username: str, first_name: str, text: str):
 
 @disp.message(CommandStart())
 async def start_handler(msg: Message):
-    await msg.answer(
-        "Шановні студенти і викладачі! Ласкаво прошу  приєднатись до обговорення освітньої програми \"Інженерія програмного забезпечення\" кафедри ІПЗ ДУІТЗ 😉\n"
-        "Залишайте свої пропозиції, зауваження, міркування як до самої ОП, так і до складу вибіркових дисциплін, а також взагалі - до всієї системи освіти в ДУІТЗ. З повагою та очікуваннями Чат-бот кафедри ІПЗ.",
+    await msg.answer_photo(
+        photo=types.FSInputFile("img/menu_img.jpg"),
+        caption="Шановні студенти і викладачі! Ласкаво прошу  приєднатись до обговорення освітньої програми \"Інженерія програмного забезпечення\" кафедри ІПЗ ДУІТЗ 😉\n"
+                "Залишайте свої пропозиції, зауваження, міркування як до самої ОП, так і до складу вибіркових дисциплін, а також взагалі - до всієї системи освіти в ДУІТЗ. З повагою та очікуваннями Чат-бот кафедри ІПЗ.",
+        reply_markup=menu_markup,
+    )
+
+@disp.message(F.text == "☰ Меню ☰")
+async def menu_handler(msg: Message):
+    await msg.answer_photo(
+        photo=types.FSInputFile("img/menu_img.jpg"),
+        caption="☰ Меню ☰",
         reply_markup=main_keyboard_markup,
     )
 
@@ -65,7 +82,7 @@ async def save_feedback_handler(msg: Message, state:FSMContext):
     username = msg.from_user.username
 
     await save_feedback(username, first_name, text)
-    await msg.answer("Дякую за ваш відгук!", reply_markup=main_keyboard_markup)
+    await msg.answer("Дякую за ваш відгук!", reply_markup=menu_markup)
 
 async def main():
     await disp.start_polling(bot)
